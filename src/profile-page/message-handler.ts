aafd074cc6b558592424244e472979c51ce0b131
Gawain Lynch <gawain.lynch@gmail.com>
2019-02-21 17:32:02 +0100

    Simplify exit logic

--- src/profile-page/message-handler.ts
+++ src/profile-page/message-handler.ts
@@ -2,15 +2,12 @@ import { IProfile } from '../interfaces';
 
 export class MessageHandler {
     public static onPostParams(event: MessageEvent): null | { profile: IProfile, token: string } {
-        if (event.data.action === 'POST_PARAMS') {
-            window.removeEventListener('message', MessageHandler.onPostParams, false);
+        if (event.data.action !== 'POST_PARAMS') { return null; }
+        window.removeEventListener('message', MessageHandler.onPostParams, false);
 
-            return {
-                profile: event.data.payload.profile.profile,
-                token: event.data.payload.token,
-            };
-        }
-
-        return null;
+        return {
+            profile: event.data.payload.profile.profile,
+            token: event.data.payload.token,
+        };
     }
 }

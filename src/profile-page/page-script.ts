c95402a120006775dfad0aaed3294e074fbbab75
Gawain Lynch <gawain.lynch@gmail.com>
2019-02-22 08:17:02 +0100

    Don't post profileParams if undefined

--- src/profile-page/page-script.ts
+++ src/profile-page/page-script.ts
@@ -2,6 +2,7 @@ declare var ACCESS_TOKEN;
 declare var profileParams;
 
 window.addEventListener('load', (event: Event) => {
+    if (typeof profileParams === 'undefined') { return; }
     window.postMessage({action: 'POST_PARAMS', payload: {
             profile: profileParams,
             token: ACCESS_TOKEN,

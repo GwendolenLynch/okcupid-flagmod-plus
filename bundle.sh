6981a29023c2d24a251cfa6755bb1fd786756627
Gawain Lynch <gawain.lynch@gmail.com>
2019-02-22 08:41:59 +0100

    Update bundle.sh to package a source zip & build ES

--- bundle.sh
+++ bundle.sh
@@ -2,6 +2,7 @@
 
 VERSION=$1
 ZIP_FILE="okcupid-flagmod-plus-${VERSION}.zip"
+SRC_FILE="okcupid-flagmod-plus-source.zip"
 PROJECT_DIR="$(pwd)"
 OUT_DIR=$(dirname "${PROJECT_DIR}")
 
@@ -10,14 +11,19 @@ if [[ ${VERSION} == "" ]] ; then
     exit 1
 fi
 
+yarn install
+yarn build
+
 pushd dist/ 2>&1 > /dev/null
 
-echo ${VERSION}
-echo ${PROJECT_DIR}
-echo ${OUT_DIR}
+echo "Creating version: ${VERSION}"
+echo "Project directory: ${PROJECT_DIR}"
+echo "Output directory: ${OUT_DIR}"
 
 rm -f "${OUT_DIR}/${ZIP_FILE}"
-
 zip -9 -r "${OUT_DIR}/${ZIP_FILE}" . --exclude app/*.json
 
 popd 2>&1 > /dev/null
+
+rm -f "${OUT_DIR}/${SRC_FILE}"
+zip -9 -r "${OUT_DIR}/${SRC_FILE}" package.json src/ scss/ dist/manifest.json dist/html/ dist/images/
